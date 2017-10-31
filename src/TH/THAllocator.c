9171c42b5c646186ff1fbb9a27f0ff62abd2d8eb
Zach DeVito <zdevito@fb.com>
2017-10-31 12:07:14 -0700

    fix issues with TH_ATOMIC_INT_LOCK_FREE

--- src/TH/THAllocator.c
+++ src/TH/THAllocator.c
@@ -1,6 +1,17 @@
 #include "THAllocator.h"
 #include "THAtomic.h"
 
+/* needed for ATOMIC_INT_LOCK_FREE */
+/* cannot go in THAtomic.h because of interactions with OpenMP giving
+   sorry not implemented errors */
+#include <stdatomic.h>
+#if defined(USE_C11_ATOMICS) && defined(ATOMIC_INT_LOCK_FREE) && \
+  ATOMIC_INT_LOCK_FREE == 2
+#define TH_ATOMIC_IPC_REFCOUNT 1
+#elif defined(USE_MSC_ATOMICS) || defined(USE_GCC_ATOMICS)
+#define TH_ATOMIC_IPC_REFCOUNT 1
+#endif
+
 /* stuff for mapped files */
 #ifdef _WIN32
 #include <windows.h>
